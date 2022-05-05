24f806315c5f0570e47f3ecb2df84f9db2f5a9db
danillyamkin <danilwebdev@gmail.com>
2022-05-06 01:36:51 +0300

    Refactor

--- src/components/ui/CarsListItem/CarsListItem.tsx
+++ src/components/ui/CarsListItem/CarsListItem.tsx
@@ -1,13 +1,12 @@
 import React, { FunctionComponent } from 'react';
 
 import classes from './CarsListItem.module.scss';
-import CarManufacturer from '../CarManufacturer/CarManufacturer';
 import CarSpecificationsLine from '../CarSpecificationsLine/CarSpecificationsLine';
 import CarPrice from '../CarPrice/CarPrice';
 import NoImage from '../NoImage/NoImage';
 import { Car } from '../../../services/car/models/car';
 import { mileageFormatter } from '../../../helpers/mileageFormatter';
-import { CARS_ROUTE, MANUFACTURERS_ROUTE } from '../../../constants/routes';
+import { CARS_ROUTE } from '../../../constants/routes';
 import { STATIC_URL } from '../../../http';
 import { Link } from 'react-router-dom';
 
@@ -22,25 +21,22 @@ const CarsListItem: FunctionComponent<Props> = ({ car }): JSX.Element => {
     id,
     name,
     price,
-    color,
     drive,
     mileage,
     transmission,
-    manufacturer,
     images,
   } = car;
   const specifications: string[] = [
-    `${mileageFormatter.format(mileage)} км`,
-    color,
     drive,
     transmission,
   ];
 
   return (
     <div className={classes.car}>
-      <Link to={`${CARS_ROUTE}/${id}`} className={classes.image}>
+      <Link to={`${CARS_ROUTE}/${id}`} className={classes.imageLink}>
         {images && images[0] ? (
           <img
+            className={classes.image}
             src={`${STATIC_URL}/${images[0].fileName}`}
             alt='car'
           />
@@ -54,16 +50,13 @@ const CarsListItem: FunctionComponent<Props> = ({ car }): JSX.Element => {
           {name}
         </Link>
 
+        <span className={classes.mileage}>
+          {`${mileageFormatter.format(mileage)} км`}
+        </span>
+
         <div className={classes.specifications}>
           <CarSpecificationsLine specifications={specifications} />
         </div>
-
-        <Link to={`${MANUFACTURERS_ROUTE}/${manufacturer.id}`} className={classes.manufacturer}>
-          <CarManufacturer
-            logo={`${STATIC_URL}/${manufacturer.logo}`}
-            name={manufacturer.name}
-          />
-        </Link>
 
         <div className={classes.price}>
           <CarPrice price={price} />
