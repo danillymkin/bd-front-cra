5ff606f44a4fb928ade510c1be37246c48a021ea
danillyamkin <danilwebdev@gmail.com>
2022-05-06 16:02:51 +0300

    Added adaptive

--- src/components/layouts/DashboardLayout/DashboardLayout.tsx
+++ src/components/layouts/DashboardLayout/DashboardLayout.tsx
@@ -4,6 +4,7 @@ import classes from './DashboardLayout.module.scss';
 import Sidebar from '../../widgets/Sidebar/Sidebar';
 import Header from '../../widgets/Header/Header';
 import Footer from '../../widgets/Footer/Footer';
+import { useMediaQuery } from 'usehooks-ts';
 
 interface DashboardLayoutProps {
   pageTitle: string;
@@ -16,12 +17,14 @@ const DashboardLayout: FunctionComponent<Props> = ({
                                                      pageTitle,
                                                      children,
                                                    }): JSX.Element => {
+  const matchesSm = useMediaQuery('(min-width: 640px)');
+
   return (
     <div className={classes.layout}>
       <Sidebar />
 
       <main className={classes.main}>
-        <Header pageTitle={pageTitle} />
+        <Header pageTitle={pageTitle} isMobile={!matchesSm} />
 
         <div className={classes.content}>{children}</div>
 
