4fd5c0ec51bd9eb1371170c23937cc742cb43959
danillyamkin <danilwebdev@gmail.com>
2022-05-06 16:00:42 +0300

    Added adaptive

--- src/components/widgets/Header/Header.tsx
+++ src/components/widgets/Header/Header.tsx
@@ -3,31 +3,34 @@ import { Link } from 'react-router-dom';
 
 import classes from './Header.module.scss';
 import { LOGIN_ROUTE } from '../../../constants/routes';
-import { useTypedSelector } from '../../../hooks/useTypedSelector';
 import HeaderUser from '../../ui/HeaderUser/HeaderUser';
+import { useTypedSelector } from '../../../hooks/useTypedSelector';
 
 interface HeaderProps {
   pageTitle: string;
+  isMobile?: boolean;
 }
 
 type Props = HeaderProps;
 
-const Header: FunctionComponent<Props> = ({ pageTitle }): JSX.Element => {
+const Header: FunctionComponent<Props> = ({ pageTitle, isMobile = false }): JSX.Element => {
   const { user } = useTypedSelector((state) => state.auth);
 
   return (
     <header className={classes.header}>
       <h1 className={classes.pageTitle}>{pageTitle}</h1>
 
-      <div>
-        {user ? (
-          <HeaderUser user={user} />
-        ) : (
-          <Link to={LOGIN_ROUTE} className={classes.login}>
-            Войти
-          </Link>
-        )}
-      </div>
+      {!isMobile && (
+        <div>
+          {user ? (
+            <HeaderUser user={user} />
+          ) : (
+            <Link to={LOGIN_ROUTE} className={classes.login}>
+              Войти
+            </Link>
+          )}
+        </div>
+      )}
     </header>
   );
 };
